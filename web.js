ae61fc9879412926d8e82acf893d40ea8045fa18
Janusz Lenar <malleor@gmail.com>
2013-08-07 00:19:59 +0200

    Extra route

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/:name', function(request, response) {
+  response.send('Hello '+request.params.name+'!');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
