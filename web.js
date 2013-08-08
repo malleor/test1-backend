88480b5490b3890abadfaad914baf5786a64ca66
malleor <malleor@gmail.com>
2013-08-08 23:24:06 +0200

    added an 'api' route to support dynamic requests

--- web.js
+++ web.js
@@ -1,6 +1,22 @@
 var express = require("express");
 var app = express();
+
+
+
+var dataset = {
+	ala: 'Ala ma kota.',
+	jacek: 'Jacek ma dinozaura.',
+	szymon: 'Szymon ma bzika.'
+};
+
+
+// middleware
+
 app.use(express.logger());
+//app.use(express.session());
+
+
+// routes
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -10,11 +26,20 @@ app.get('/hello/:name', function(request, response) {
   response.send('Hello '+request.params.name+'!');
 });
 
-
 app.get('/data.json', function(request, response) {
   response.header('Access-Control-Allow-Origin', '*');
   response.json({hello: 'Hi there.'});
 });
+
+app.get('/api/:name', function(request, response) {
+  response.header('Access-Control-Allow-Origin', '*');
+  var name = request.params.name;
+  var ans = dataset[name];
+  response.json({ ans: ans });
+});
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
